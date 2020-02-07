ca266672e153cf6ba9bdc1d587397dc8491b5485
intelligentgoldfish <50779300+intelligentgoldfish@users.noreply.github.com>
2020-02-06 21:13:12 -0600

    Modified activation function and learning rate
    Network now utilizes cross-entropy cost function to determine gradients, and performs best with a small learning rate around 0.1.

--- assembleNetwork.m
+++ assembleNetwork.m
@@ -1,16 +1,14 @@
 %%assembleNetwork.m
 %%Command-line code for creating and testing neural network
-%%Version 3.2.2
+%%Version 3.3.1
 %%Author: Thomas DeWitt
 
 clear; clc;
 
 disp('~~~MNIST-Trainable Feedforward Neural Network~~~');
 disp('Author: Thomas DeWitt');
-disp('Version 3.2.2');
-disp(['Updates:',newline,'-Tweaked and smoothed initialization for faster learning',newline,...
-    '-Removed extraneous code and commenting',newline,...
-    '-Streamlined and condensed existing code',newline,newline,...
+disp('Version 3.9.2');
+disp(['Updates:',newline,'-Now using the swish activation function',newline,newline,...
     'Press any key to create and train a new network.',newline]);
 
 pause;
@@ -20,7 +18,7 @@ disp('Initializing...');
 dim = [784 35 10];
 epochs = 20;
 miniBatchSize = 10;
-learningRate = 3;
+learningRate = 0.1;
 
 [w,b] = initNetwork3(dim);
 
